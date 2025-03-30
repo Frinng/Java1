f843502770d922720beb9432048509e3958e0a5b
Frinng <167120714+Frinng@users.noreply.github.com>
2025-03-30 12:37:53 -0300

    Server como base para um loop

--- src/Atividade23.java
+++ src/Atividade23.java
@@ -3,34 +3,39 @@ import java.util.Scanner;
 public class Atividade23 {
     public static void main(String[] args){
         var entrada = new Scanner(System.in);
+        boolean continuar = true;
+        String Brasilia = "Brasília";
+        String CapitalMg = "Belo Horizonte";
+        String CapitalSp = "São Paulo";
+        String CapitalRj = "Rio De Janeiro";
 
-        System.out.println("---------Desafio de Geografia---------");
-        System.out.println("Você gostaria de participar?");
-        System.out.println("1-Sim");
-        System.out.println("2-Não");
-        String escolha = entrada.nextLine();
 
-        if (escolha.equals("1")){
-            System.out.println("Então vamos começar.....");
-            System.out.println("Qual a capital do Brasil?");
-            String capital = entrada.nextLine();
 
-        } else if (escolha.equals("2")) {
-            System.out.println("Você deseja sair do sistema? ");
-            String escolha2 = entrada.nextLine();
 
-            if (escolha2.equals("Sim") || escolha2.equals("sim") || escolha2.equals("SIM")){
-                System.out.println("Saindo do Sistema......");
-                System.exit(0);
-            }else {
-                System.out.println("Retornando ao inicio");
+        while (continuar){
+            System.out.println("-------Desafio de Geografia-------");
+            System.out.println("Você gostaria de participar?");
+            System.out.println("1-Sim");
+            System.out.println("2-Não");
+            String escolha = entrada.nextLine();
 
+            if (escolha.equals("1")){
+                System.out.println("Então vamos la ;) ");
+                System.out.println("Qual a capital do Brasil? ");
+                String capitalBrasil = entrada.nextLine();
+
+            } else if (escolha.equals("2")) {
+                System.out.println("Você deseja sair do sistema?");
+                String escolha2 = entrada.nextLine();
+
+                if (escolha2.equals("Sim")  || escolha2.equals("sim") || escolha2.equals("SIM")){
+                    continuar = false;
+                    System.out.println("Saindo do Sistema...");
+                }else {
+                    System.out.println("Retornando ao inicio");
+                }
 
             }
-        }else {
-            System.out.println("DIGITE 1 para sim e 2 para não");
         }
-
-
     }
 }
